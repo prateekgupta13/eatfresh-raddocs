f0e0e71a6ccbe9dd36d8b7a50d1ca37e8d09bf4e
Prateek Gupta <prateek@akaruilabs.com>
2013-09-18 14:07:13 +0530

    changed app name to Eathalal API Documentation

--- lib/raddocs/configuration.rb
+++ lib/raddocs/configuration.rb
@@ -15,7 +15,7 @@ module Raddocs
 
     add_setting :docs_dir, :default => "docs"
     add_setting :docs_mime_type, :default => /text\/docs\+plain/
-    add_setting :api_name, :default => "Api Documentation"
+    add_setting :api_name, :default => "Eathalal Api Documentation"
     add_setting :include_bootstrap, :default => true
     add_setting :external_css, :default => []
 
